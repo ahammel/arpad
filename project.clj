dc3b84d97d84ed1111b3ed488f5e220a949a4356
Alex Hammel <ahammel87@gmail.com>
2015-07-12 17:02:36 -0700

    Bump to Clojure 1.7.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ahammel/arpad"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [prismatic/schema "0.4.3"]
