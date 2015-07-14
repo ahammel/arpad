cd6cec5b37575924545bd5494b8b7808990db0b7
Alex Hammel <ahammel87@gmail.com>
2015-07-13 21:16:26 -0700

    Create pool state file if it doesn't exist
           * Modified arpad.persistor/persist to append an empty string to
             the output file before writing to it. Prevents 'file does not
             exist' errors.

--- src/arpad/persistor.clj
+++ src/arpad/persistor.clj
@@ -5,8 +5,9 @@
 
 (defn persist
   "Write the target string to a file, moving the original file to
-  'filename.bak'."
+  'filename.bak'. Creates the file if it does not exist."
   [filename pool]
+  (spit filename "" :append true)
   (let [f (file filename)
         bak (file (str filename ".bak"))
         pool-json (json/write-str pool)]
