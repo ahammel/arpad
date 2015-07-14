748e2fb85f1720c385197f2deeb3ecc87a876d65
Alex Hammel <ahammel87@gmail.com>
2015-07-13 19:45:44 -0700

    Make blocking persistor
         * Added 'run-persistor' function, which is a thread-blocking
           version of 'spawn-persistor'.

--- src/arpad/persistor.clj
+++ src/arpad/persistor.clj
@@ -1,7 +1,7 @@
 (ns arpad.persistor
   (:require [clojure.java.io    :refer [file]]
             [clojure.data.json  :as json]
-            [clojure.core.async :refer [<! go-loop]]))
+            [clojure.core.async :refer [<! <!! go-loop]]))
 
 (defn persist
   "Write the target string to a file, moving the original file to
@@ -14,7 +14,18 @@
     (spit filename pool-json)))
 
 (defn spawn-persistor
+  "Spawn a process which persists a pool to a given OS file."
   [filename chan]
   (go-loop [pool (<! chan)]
     (when pool
-      (persist filename pool))))
+      (persist filename pool)
+      (recur (<! chan)))))
+
+(defn run-persistor
+  "Blocking version of spawn-persistor"
+  [filename chan]
+  (loop [pool (<!! chan)]
+    (when pool
+      (persist filename pool)
+      (recur (<!! chan)))))
+
